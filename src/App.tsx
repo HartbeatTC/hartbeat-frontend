e7a0b81ac6a6e4ceb802aed68fbd33e9bdfaea2f
Nick Aguila <aguilawebdev@gmail.com>
2023-09-12 15:32:40 -0400

    start of hb frontend repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav> NAVBAR</nav>
+      <main> MAIN BODY</main>
+      <footer> FOOTER </footer>
+    </>
+  );
+}
+
+export default App;
